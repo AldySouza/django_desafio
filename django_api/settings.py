aebccea1e4f00a32dc920c543a15c22e6a0fc8f7
Aldy Souza <60198926+AldySouza@users.noreply.github.com>
2021-08-02 22:05:40 -0300

    Update settings.py

--- django_api/settings.py
+++ django_api/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'api',
         'USER': 'root',
-        'PASSWORD': 'mzhide',
+        'PASSWORD': '****',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
